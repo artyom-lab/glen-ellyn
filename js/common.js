897d38af485937d2707e8b26f35a3c4901dccebf
artyom-lab <artyomgalkeane@gmail.com>
2023-09-01 19:02:51 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,23 @@
+$(function() {
+
+  $('.owl-1').owlCarousel({
+  	responsive: {
+  		0: {
+  			items: 1,
+  		},
+  		400: {
+  			items: 1,
+  		},
+  		600: {
+  			items: 1,
+  		},
+  	},
+	  smartSpeed: 1200,
+	  nav: true,
+	  dots: false,
+	  navText: ['','']
+  });
+
+});
+
+
