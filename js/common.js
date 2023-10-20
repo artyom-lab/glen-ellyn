485417d25d74ec68b05e753db80a58c5964d2168
artyom-lab <artyomgalkeane@gmail.com>
2023-10-20 19:37:39 +0300

    34543

--- js/common.js
+++ js/common.js
@@ -1,5 +1,23 @@
 $(function() {
 
+    $(document).on('click.bs.dropdown.data-api', '.dropdown-menu', function (e) { 
+    e.stopPropagation();
+  });
+
+$("#sandwich").on("click", function() { 
+  $(".navbar").toggleClass("navbar-white");
+});
+
+  function navbar() {
+    if ($(this).scrollTop() >= $(window).height()/2) {
+      $('.nav-white').addClass("sticky");
+    } else {
+      $('.nav-white').removeClass("sticky");
+    };
+  };
+  navbar();
+  $(window).on("scroll", navbar);
+
   $('.owl-1').owlCarousel({
   	responsive: {
   		0: {
